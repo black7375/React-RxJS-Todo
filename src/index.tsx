5f08613051c619af4947170aa48efc2d98df7339
alstjr7375 <alstjr7375@daum.net>
2020-04-29 08:26:11 +0900

    Add: normalize css

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import './Generic/normalize.scss';
 
 ReactDOM.render(
   <React.StrictMode>
