273f0d838f3cf920a8fdfeb7b24fdca25b89ed07
alstjr7375 <alstjr7375@daum.net>
2020-05-04 06:54:07 +0900

    Clean: Remove unnecessary codes

--- src/Tools/Tools.ts
+++ src/Tools/Tools.ts
@@ -1,33 +1,4 @@
-import { useState, useEffect } from 'react';
-import { Observable } from 'rxjs';
 import classNames from 'classnames/bind';
-
-// == RxJS =====================================================================
-export function useObservable<T>(
-  observable: Observable<T>
-  ) : T | undefined;
-export function useObservable<T, K = keyof T>(
-  observable: Observable<T>,
-  init?:      K,
-  callback?:  (state: T) => K
-): K;
-
-export function useObservable<T, K = keyof T> (
-  observable: Observable<T>,
-  init?:      K,
-  callback?:  (state: T) => K): T | K | undefined {
-  const [state, setState] = useState<T | K>(init!);
-
-  useEffect(() => {
-    const sub = callback
-              ? observable.subscribe((data: K | any) => setState(callback(data)))
-              : observable.subscribe(setState);
-    return () => sub.unsubscribe();
-  }, [observable, callback]);
-
-  return state;
-};
-
 
 // == Style ====================================================================
 type stylesT = {
